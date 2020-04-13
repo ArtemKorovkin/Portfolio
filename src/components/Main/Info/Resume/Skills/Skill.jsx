55636d8456045a12cb593167ca8d89dc3b88e460
ArtemKorovkin <art888kor@gmail.com>
2020-04-13 19:19:19 +1000

    Work on section resume skills

--- src/components/Main/Info/Resume/Skills/Skill.jsx
+++ src/components/Main/Info/Resume/Skills/Skill.jsx
@@ -3,7 +3,23 @@ import style from './skill.module.scss'
 
 const Skills  = (props) => {
   return (
-    <div className={style.skills}></div>
+    <div className={style.skills}>
+      <div className={style.title}>
+        Skills
+      </div>
+      <div className={style.demonstration}>
+        <p className={style.text}>
+
+        </p>
+        <span className={style.html}>HTML</span>
+        <div></div>
+        <span className={style.css}>CSS</span>
+        <div></div>
+        <span className={style.javaScript}>JavaScript</span>
+        <div></div>
+        <span className={style.react}>React</span>
+      </div>
+    </div>
   )
 }
 
