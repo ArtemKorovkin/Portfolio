49a8ee980399bbd1c0c5df78b25edda0febf56a1
ArtemKorovkin <art888kor@gmail.com>
2020-06-05 14:32:40 +1000

    Add new information about me

--- src/components/Header/Body/Body.jsx
+++ src/components/Header/Body/Body.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import s from './body.module.scss'
-import Social from './Social/Social'
-import Btn from '../Button/Button'
+import React from "react";
+import s from "./body.module.scss";
+import Social from "./Social/Social";
 
-const Body = (props) => {
+const Body = props => {
   return (
-      <div className={s.body}>
-        <h1 className={s.title}>Я Коровкин Артем.</h1>
-        <p className={s.text}>
-          Я - ученик IT школы. Прохожу курсы на позицию <span>frontend developer.</span> Очень увлекся этой специальностью, особенно интересна разработка на React. На учебу стараюсь выделять все свободное время. Изучаю много новой информации, но я знаю, что все старания не зря и <span>я буду полезен людям!</span>
-        </p>
-        <Social/>
-      </div>
-)
-}
+    <div className={s.body}>
+      <h1 className={s.title}>Я Коровкин Артем.</h1>
+      <p className={s.text}>
+        Я - ученик IT школы. Прошел курсы на позицию
+        <span> frontend developer.</span> Очень увлекся этой специальностью,
+        особенно интересна разработка на <span>React.</span> Свободное время
+        уделяю изучению новой информации, а так же интересуюсь новыми
+        технологиями в сфере веб-разработки. В настоящее время являюсь
+        сотрудником <span>IT компании Atom.</span> В компании нахожусь на
+        должности верстальщика, работаю в системе Bitrix24. Моя главная цель в
+        этой профессии - <span>стать востребованным специалистом!!!</span>
+      </p>
+      <Social />
+    </div>
+  );
+};
 
-export default Body
+export default Body;
--- src/components/Header/Navigation/Nav.jsx
+++ src/components/Header/Navigation/Nav.jsx
@@ -1,20 +1,64 @@
-import React from 'react';
-import style from './nav.module.scss'
-import Burger from './BurgerMenu/Burger'
+import React from "react";
+import style from "./nav.module.scss";
+import Burger from "./BurgerMenu/Burger";
+import { Link } from "react-scroll";
 
-
-const Navigation  = (props) => {
+const Navigation = props => {
   return (
     <nav className={style.nav}>
       <ul className={style.menuList}>
-        <li className={style.menuItem}><a href=''>Главная</a></li>
-        <li className={style.menuItem}><a href=''>О себе</a></li>
-        <li className={style.menuItem}><a href=''>Резюме</a></li>
-        <li className={style.menuItem}><a href=''>Работы</a></li>
+        <li className={style.menuItem}>
+          <Link
+            to="mainPage"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+            activeClass={style.activeLink}
+          >
+            Главная
+          </Link>
+        </li>
+        <li className={style.menuItem}>
+          <Link
+            to="aboutMe"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+            activeClass={style.activeLink}
+          >
+            О себе
+          </Link>
+        </li>
+        <li className={style.menuItem}>
+          <Link
+            to="myResume"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+            activeClass={style.activeLink}
+          >
+            Резюме
+          </Link>
+        </li>
+        <li className={style.menuItem}>
+          <Link
+            to="myWorks"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+            activeClass={style.activeLink}
+          >
+            Работы
+          </Link>
+        </li>
       </ul>
-      <Burger/>
+      <Burger />
     </nav>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
